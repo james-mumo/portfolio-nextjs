1e38ef6e2f6f5e561513f84d970069c01d46834c
mumo98 <mumojames98@gmail.com>
2023-02-17 13:16:59 +0300

    latest commit

--- app/components/SideLinks/page.js
+++ app/components/SideLinks/page.js
@@ -9,7 +9,7 @@ import "./sidelinks.css"
 const SideLinks = () => {
   return (
     <div className="container flex flex-row justify-between bottom-0 border w-[100vw] mr-0">
-      <div className="leftSpan">[&#xf09b;]</div>
+      <div className="leftSpan">gh</div>
       <div className="rightSpan">
         <a href="mailto:mumojames98@gmail.com" className="mailTo">
           mumojames98@gmail.com
